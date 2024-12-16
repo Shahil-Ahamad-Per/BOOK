8b331c3b1f227678831152c30f674f6d38327296
Shahil-Ahamad-Per <shahilahamad04@gmail.com>
2024-12-16 10:51:06 +0545

    Deploy

---
+++ backend/src/utils/config.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+
+config({
+  path: ".env",
+});
+
+export const env = {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: Number(process.env.PORT) || 4000,
+  MONGO_URI: process.env.MONGO_URI || "",
+  JWT_SECRET: process.env.JWT_SECRET || "",
+  FRONTEND_URL: process.env.FRONTEND_URL || "",
+} as const;
